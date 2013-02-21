ae2fb8d8c44bef0e5010a5c7de253d7aa056d9d6
Mikhail Rybalkin <rybalkin@ggasoftware.com>
2013-02-21 18:17:07 +0400

    imago: copyright update

--- imago/src/filters_list.cpp
+++ imago/src/filters_list.cpp
@@ -1,4 +1,18 @@
-#include "filters_list.h"
+/****************************************************************************
+ * Copyright (C) 2009-2013 GGA Software Services LLC
+ *
+ * This file is part of Imago OCR project.
+ *
+ * This file may be distributed and/or modified under the terms of the
+ * GNU General Public License version 3 as published by the Free Software
+ * Foundation and appearing in the file LICENSE.GPL included in the
+ * packaging of this file.
+ *
+ * This file is provided AS IS with NO WARRANTY OF ANY KIND, INCLUDING THE
+ * WARRANTY OF DESIGN, MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
+ ***************************************************************************/
+
+ #include "filters_list.h"
 #include "prefilter_basic.h"
 #include "prefilter_retinex.h"
 
